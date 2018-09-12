0a22b1d7807451f63753b908f17ea466bbcc23a3
Jake Marzitelli <mjakehs@gmail.com>
2018-09-11 19:32:18 -0500

    add database connection module

---
+++ server/modules/database-connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const databaseUrl = 'mongodb://localhost:27017/monstermash';
+
+mongoose.connect(databaseUrl, {useNewUrlParser: true});
+mongoose.connection.once('connected', () => {
+    console.log('Connected to database at: ', databaseUrl);
+});
+mongoose.connection.on('error', (error) => {
+    console.log('Error: ', error);
+});
